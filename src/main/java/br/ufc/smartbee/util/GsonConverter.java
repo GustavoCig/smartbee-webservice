4dd3b09d905e505036407c9a31dd0bd13c8ee9da
alissonlimasilva <alissonlima.igt@gmail.com>
2017-08-19 17:13:51 -0300

    Código mais enxuto
    Mudanças nas classes para melhorar o aproveitamento de código.

--- src/main/java/br/ufc/smartbee/util/GsonConverter.java
+++ src/main/java/br/ufc/smartbee/util/GsonConverter.java
@@ -4,17 +4,14 @@ import java.util.List;
 
 import com.google.gson.Gson;
 
-import br.ufc.smartbee.modelo.Colmeias_Cadastradas;
-
 public class GsonConverter {
 
-	
 	public String converteListadeObjetos(List<?> lista) {
 		return new Gson().toJson(lista);
 	}
-	
-	public String converteColmeia(Colmeias_Cadastradas cc) {
-		return new Gson().toJson(cc);
+
+	public String converteColmeias(Object object) {
+		return new Gson().toJson(object);
 	}
 
 }
